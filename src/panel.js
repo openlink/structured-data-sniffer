e6ad4904ee748702657b1cf37a51294f50b5cf96
Sergey Malinin <smalinin@openlinksw.com>
2016-04-22 19:14:36 +1000

    Fixed issue with download Nano data.

--- src/panel.js
+++ src/panel.js
@@ -658,7 +658,7 @@ function Download_exec()
     filename = "jsonld_data.jsonld";
     fmt = "json";
   }
-  else if (selectedTab==="#turtle" && (gData.turtle.ttl_text!==null || gData.nano.ttl_text!==null)) {
+  else if (selectedTab==="#turtle" && (gData.turtle.ttl_text!==null || gData.t_nano.ttl_text!==null)) {
     filename = "turtle_data.ttl";
     fmt = "ttl";
   }
