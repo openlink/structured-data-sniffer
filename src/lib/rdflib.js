d87d9b4e8c11e11f3c4c14f0de9b0ac12e65499d
Sergey Malinin <sergmalinin@gmail.com>
2019-02-18 21:31:01 +1000

    Fix bugs
    - in rdflib.js
    - in export data as
    - fix missprints

--- src/lib/rdflib.js
+++ src/lib/rdflib.js
@@ -30774,7 +30774,9 @@ function (_Formula) {
           }
         }
 
-        this.add(small, this.sym('http://www.w3.org/2007/ont/link#uri'), big.uri); // If two things are equal, and one is requested, we should request the other.
+        if (small && small.termType==='NamedNode') {
+          this.add(small, this.sym('http://www.w3.org/2007/ont/link#uri'), big.uri); // If two things are equal, and one is requested, we should request the other.
+        }
 
         if (this.fetcher) {
           this.fetcher.nowKnownAs(big, small);
